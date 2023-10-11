c2f56881d7ef42f570edd5919e9c09fc5bc2e399
AlbertoMNTactico <utm20040077@utma.edu.mx>
2023-10-10 19:25:09 -0600

    added function index

--- src/components/Listitem.jsx
+++ src/components/Listitem.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
 function Listitem({text, onDeleted}){
+    
     const [checked, setChecked] = useState(false);
 
     const handleCheck = () => {
        setChecked(!checked);
     }
-    return (<li className={`p-2 bg-indigo-950 ${checked ? 'line-through' : null}`}>
+    return (<li className={`p-2 rounded-md bg-indigo-950 ${checked ? 'line-through' : null}`}>
         {text} <input type="checkbox" checked = {checked} onChange={handleCheck}/> </li>)
 }
-export default Listitem;
+export default Listitem; 
--- src/pages/todo.jsx
+++ src/pages/todo.jsx
@@ -1,12 +1,37 @@
+
+import { useState, useRef } from "react";
 import Listitem from "../components/Listitem";
 
 function Todo(){
+    //useState nos devuelve un array
+    const [todos, setTodos] = useState([]);
+    const inputRef = useRef(null);
+
+    const addTodo = () => {
+        const todoValue = inputRef.current.value;
+        console.log(inputRef);
+        setTodos([...todos, todoValue])
+    }
     return (
-        <div>
-            <ul>
-                <Listitem text="item 1" checked={true}></Listitem>
-                <Listitem text="item 2" checked={true}></Listitem>
-                <Listitem text="item 4" checked={false}></Listitem>
+        <div className="flex flex-col gap-2">
+            
+                <div className="flex gap-2">
+                    <input 
+                    ref={inputRef} 
+                    type="text" 
+                    className="bg-slate-400 rounded-md p-2" />
+                    <button 
+                    onClick={addTodo} 
+                    className="bg-indigo-600 rounded-md px-4 py-2">
+                    ADD Todo
+                    </button>  
+                </div> 
+            <ul className="flex flex-col gap-2">
+                {
+                    todos.map((value, index)=>{
+                        return <Listitem key={index} text={value}/>;
+                    })
+                }
             </ul>
         </div>
         )
